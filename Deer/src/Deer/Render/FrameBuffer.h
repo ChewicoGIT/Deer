07c343b3e64f8612632455e04ebe7da408d65fab
ChewicoGIT <153228935+ChewicoGIT@users.noreply.github.com>
2024-09-15 22:52:53 +0200

    Fucking shit of git that deletes my work

--- Deer/src/Deer/Render/FrameBuffer.h
+++ Deer/src/Deer/Render/FrameBuffer.h
@@ -2,20 +2,29 @@
 #include "Deer/Core/Core.h"
 #include "Deer/Core/Log.h"
 
+#include <vector>
+#include <initializer_list>
+
 namespace Deer {
+	enum class TextureBufferType {
+		RGBA8,
+		RED_INTEGER
+	};
+
 	struct FrameBufferSpecification {
 		unsigned int width, height;
 		unsigned int samples;
-
+		std::vector<TextureBufferType> frameBufferTextures;
+		 
 		bool swapChainTarget = false;
-		FrameBufferSpecification(unsigned int _width, unsigned int _height, unsigned int _samples = 1, bool _swapChainTarget = false)
-			: width(_width), height(_height), samples(_samples), swapChainTarget(_swapChainTarget) {
+		FrameBufferSpecification(unsigned int _width, unsigned int _height, std::initializer_list<TextureBufferType> _frameBufferTextures, unsigned int _samples = 1, bool _swapChainTarget = false)
+			: width(_width), height(_height), samples(_samples), frameBufferTextures(_frameBufferTextures), swapChainTarget(_swapChainTarget) {
 		}
 	};
 
 	class FrameBuffer {
 	public:
-		virtual ~FrameBuffer() { DEER_CORE_TRACE("Destroying frame buffer"); }
+		virtual ~FrameBuffer() = default;
 		virtual const FrameBufferSpecification& getSpecification() = 0;
 
 		virtual void bind() = 0;
@@ -24,7 +33,8 @@ namespace Deer {
 		virtual void clear() = 0;
 		virtual void resize(unsigned int width, unsigned int height) = 0;
 
-		virtual unsigned int getColorBufferID() = 0;
+		virtual unsigned int getTextureBufferID(int id) = 0;
+		virtual void clearBuffer()
 
 		static Ref<FrameBuffer> create(const FrameBufferSpecification& spec);
 	};
