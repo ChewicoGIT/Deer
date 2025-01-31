b2670bf5d75a59d9c041ee3aad6c1dfb1d0102f2
ChewicoGIT <153228935+ChewicoGIT@users.noreply.github.com>
2025-01-31 14:31:45 +0100

    Terrain editor

--- DeerStudio/src/DeerStudio/Editor/TerrainEditor.cpp
+++ DeerStudio/src/DeerStudio/Editor/TerrainEditor.cpp
@@ -4,6 +4,7 @@
 #include "Deer/Core/Project.h"
 #include "Deer/Scene/Scene.h"
 #include "Deer/Voxels/VoxelWorld.h"
+#include "Deer/Voxels/VoxelWorldProps.h"
 
 #include "imgui.h"
 
@@ -18,6 +19,8 @@ namespace Deer {
 
 			ImGui::Text("Voxel world is not initialized");
 
+			ImGui::Separator();
+
 			ImGui::Spacing();
 			static int values[3] = { 1 };
 			ImGui::InputInt3("Chunk Size", values);
@@ -28,30 +31,20 @@ namespace Deer {
 				else if (values[i] > 32)
 					values[i] = 32;
 
-			ImGui::Text("voxels x: ");
+			ImGui::Text("Total chunks: ");
 			ImGui::SameLine();
-			ImGui::Text(std::to_string(values[0] * 32).c_str());
-			ImGui::SameLine();
-			ImGui::Text(", y: ");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(values[1] * 32).c_str());
-			ImGui::SameLine();
-			ImGui::Text(", z: ");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(values[2] * 32).c_str());
-
-			ImGui::Text("Total voxels: ");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(values[0] * values[1] * values[2] * 32 * 32 * 32).c_str());
-
-			ImGui::Text("Theorical max size: ");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(values[0] * values[1] * values[2] * 32 * 32 * 32).c_str());
+			ImGui::Text(std::to_string(values[0] * values[1] * values[2]).c_str());
 
 			ImGui::Spacing();
 
-			if (ImGui::Button("Initialize voxel world")) {
+			if (ImGui::Button("Create voxel world")) {
+				
+				VoxelWorldProps props;
+				props.chunkSizeX = values[0];
+				props.chunkSizeY = values[1];
+				props.chunkSizeZ = values[2];
 
+				Project::m_scene.createVoxelWorld(props);
 			}
 
 			return ImGui::End();
